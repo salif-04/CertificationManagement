6c7332eabffdbbb66913d472eb9a52382250db14
salif-04 <syedsalifmoin@gmail.com>
2020-04-20 15:21:07 +0530

    Adds VoucherRecordPage Component

---
+++ force-app/main/default/lwc/voucherRecordComponent/voucherRecordComponent.js
@@ -0,0 +1,31 @@
+import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getCertificationRequests from '@salesforce/apex/Voucher.getCertificationRequests';
+
+export default class VoucherRecordComponent extends NavigationMixin(LightningElement) {
+    @api recordId;
+    certificationRequestList;
+    
+    @wire(getCertificationRequests, {recordId: '$recordId'})
+    certifiactionFetch({error, data}) {
+        if(data) {
+            this.certificationRequestList = data;
+        }
+        if(error) {
+            alert(error);
+        }
+    }
+
+    navigateToCertReq(event) {
+        event.preventDefault();
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.target.dataset.id,
+                objectApiName: 'Certification_Request__c',
+                actionName: 'view'
+            }
+        });
+    }
+    
+}
